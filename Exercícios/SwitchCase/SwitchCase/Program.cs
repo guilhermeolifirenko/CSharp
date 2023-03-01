3819f3a861dcd57ecadbdb78c9020fc205814340
Guilherme Olifirenko <guilhermeolifer21@gmail.com>
2023-03-01 14:51:26 -0300

    Uri 1037 Exercice

---
+++ Exercícios/SwitchCase/SwitchCase/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+    int x = int.Parse(Console.ReadLine());
+    string dia;
+
+    switch (x)
+    {
+        case 1:
+            dia = "domingo";
+            break;
+
+        case 2:
+            dia = "segunda";
+            break;
+
+        case 3:
+            dia = "terça";
+            break;
+
+        case 4:
+            dia = "quarta";
+            break;
+
+        case 5:
+            dia = "quinta";
+            break;
+
+        case 6:
+            dia = "sexta";
+            break;
+
+        case 7:
+            dia = "sábado";
+            break;
+
+        default:
+            dia = "valor inválido";
+            break;
+    }
+
+    Console.WriteLine("Dia da semana: " + dia);
+    Console.ReadLine();
+
+
+    // URI 1037
+    double valor = double.Parse(Console.ReadLine());
+
+    if (valor < 0.0 || valor > 100.0)
+    {
+        Console.WriteLine("Fora de intervalo");
+    }
+    else if (valor <= 25.0)
+    {
+        Console.WriteLine("Intervalo [0,25]");
+    }
+    else if (valor <= 50.0)
+    {
+        Console.WriteLine("Intervalo (25,50]");
+    }
+    else if (valor <= 75.0)
+    {
+        Console.WriteLine("Intervalo (50,75]");
+    }
+    else
+    {
+        Console.WriteLine("Intervalo (75,100]");
+    }
+*/
